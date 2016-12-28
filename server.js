b70228b2d7eef5504f1fd4add2839776123cdbcf
dstigler <S1510307109@students.fh-hagenberg.at>
2016-12-29 00:44:47 +0100

    check authentication

--- server.js
+++ server.js
@@ -49,6 +49,41 @@ app.get('/setup', function(req, res) {
 
 var apiRoutes = express.Router();
 
+apiRoutes.post('/authenticate', function(req, res) {
+
+  // find the user
+  User.findOne({
+    name: req.body.name
+  }, function(err, user) {
+
+    if (err) throw err;
+
+    if (!user) {
+      res.json({ success: false, message: 'Authentication failed. User not found.' });
+    } else if (user) {
+
+      // check if password matches
+      if (user.password != req.body.password) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+
+        // if user is found and password is right
+        // create a token
+        var token = jwt.sign(user, app.get('superSecret'), {
+          expiresInMinutes: 1440 // expires in 24 hours
+        });
+
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }
+    }
+  });
+});
+
 apiRoutes.get('/', function(req, res){
     res.json({message: 'Welcome to first version of wdp3chat-api'});
 });
