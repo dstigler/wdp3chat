a89afdd0532386e8b2e124c03fd85e21cda6d48f
dstigler <S1510307109@students.fh-hagenberg.at>
2016-12-29 15:14:11 +0100

    try

--- public/login.js
+++ public/login.js
@@ -13,6 +13,31 @@ $('.tab a').on('click', function (e) {
 
 });
 
+function sendData(){
+        //get the input value
+        $name = $('#uname').val();
+        $pwd = $('#pwd').val();
+        $.ajax({
+            //the url to send the data to
+            url: "/login",
+            //the data to send to
+            data: {name : $name, pwd: $pwd},
+            //type. for eg: GET, POST
+            type: "POST",
+            //datatype expected to get in reply form server
+            dataType: "json",
+            //on success
+            success: function(data){
+                console.log("success");//do something after something is recieved from php
+            },
+            //on error
+            error: function(){
+                //bad request
+                console.log("error");
+            }
+        });
+    }
+
 function validate() {
     var ra = document.getElementById("uname").value;
     var rag = document.getElementById("pwd").value;
