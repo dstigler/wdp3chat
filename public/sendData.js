515a95513a4eeaab9dc1e5a9e4ef82f85de62c61
dstigler <S1510307109@students.fh-hagenberg.at>
2016-12-29 16:13:36 +0100

    try

---
+++ public/sendData.js
@@ -0,0 +1,21 @@
+
+function validate() {
+    var ra = document.getElementById("uname").value;
+    var rag = document.getElementById("pwd").value;
+    $.ajax({
+       type: "POST",
+        url: "/login",
+        contentType: "application/json",
+        dataType: 'json',
+       data:JSON.stringify({
+           username:ra,
+           password:rag
+       });
+    }).
+    success( function() {
+      alert('success');
+
+    });
+    console.log(ra, rag);
+
+};
