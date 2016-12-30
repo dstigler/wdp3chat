caf1b3428957ffa66f88e78f70baff713c1ed462
dstigler <S1510307109@students.fh-hagenberg.at>
2016-12-30 18:21:51 +0100

    clean up

--- public/login.js
+++ public/login.js
@@ -12,3 +12,69 @@ $('.tab a').on('click', function (e) {
   $(target).fadeIn(600);
 
 });
+
+function createUser() {
+    var nu = document.getElementById("newname").value;
+    var ne = document.getElementById("newmail").value;
+    var np = document.getElementById("newpwd").value;
+    $.ajax({
+       type: "POST",
+        url: "/login",
+        contentType: "application/json",
+        dataType: 'json',
+       data:JSON.stringify({
+           "username":nu,
+           "email": ne,
+           "password":np
+       })
+   }),
+    function(data) {
+      console.log(data.body);
+      /*
+      localStorage.setItem('token', 'asY-x34SfYPk'); // write
+      console.log(localStorage.getItem('token'));
+      */
+    };
+    console.log(nu, ne, np);
+};
+
+//var hash = require('./pass').hash;
+
+function validateUser() {
+    var ra = document.getElementById("uname").value;
+    var rag = document.getElementById("pwd").value;
+    $.ajax({
+       type: "PUT",
+        url: "/login",
+        contentType: "application/json",
+        dataType: 'json',
+       data:JSON.stringify({
+           "username":ra,
+           "password":rag,
+           "token":"empty"
+       }),
+       success: function(data){
+           console.log('success');
+           console.log(data.token);
+       },
+       error: function(data){
+           console.log('error');
+           console.log(data);
+       }
+   });
+
+    /*function(data) {
+      console.log(data.body);
+      /*
+      localStorage.setItem('token', 'asY-x34SfYPk'); // write
+      console.log(localStorage.getItem('token'));
+      */
+    //};
+    console.log(ra, rag);
+
+}
+
+
+document.getElementById("loginBtn").onclick = validateUser;
+
+document.getElementById("createBtn").onclick = createUser;
