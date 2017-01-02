81a4c7f5aa6515a79a982d8d9d74adf53e1faa9e
dstigler <S1510307109@students.fh-hagenberg.at>
2017-01-02 12:36:48 +0100

    try

--- api.js
+++ api.js
@@ -1,72 +1,12 @@
-app.route('/login')
-    .get(function(req, res){
-        res.sendFile(path.join(__dirname, 'views/login.html' ));
-    })
-    .put(function(req, res){
-        // find the user
-        console.log('PUT: ' + req.body.username);
-
-        User.findOne({name: req.body.username}, function(err, user) {
-
-          if (err) {
-              //user not found
-              return res.sent(401);
-          }
-
-          if (!user) {
-            //incorrect username
-            return res.sendStatus(401);
-            //res.json({ success: false, message: 'Authentication failed. User not found.' });
-          }
-          if (user.password != req.body.password) {
-            //incorrect password
-            return res.sendStatus(401);
-            // check if password matches
-            //  res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-          }
-          //res.sendStatus(200);
-              // if user is found and password is right
-              // create a token
-          var token = jwt.sign(user, app.get('superSecret'), {
-            expiresIn: '1440m' // expires in 24 hours
-          });
-          console.log(token);
-          //var decoded = jwt.decode(token);
-          //console.log(decoded);
-
-        res.cookie('auth',token);
-        res.send({message: 'ok'});
-        //res.send(req.body);
-      });
-
-
-    })
-    .post(function(req, res){
-        console.log('POST: ' + req.body.username);
-        User.findOne({'name': req.body.username}, function(err, user) {
-            if (err) throw err;
-
-            var newUser = new User({
-              name: req.body.username,
-              email: req.body.email,
-              password: req.body.password,
-              admin: false
-            });
-
-            if (user) {
-                res.json({ success: false, message: 'Authentication failed. User already exists.' });
-            } else {
-                newUser.save(function(err) {
-                  if (err) throw err;
-
-                  res.json({ success: true, message: 'User saved successfully' });
-                });
-            }
-        });
-
-    });
-
 var apiRoutes = express.Router();
+
+var mongoose = require('mongoose');
+var path = require('path');
+var cookieParser = require('cookie-parser')
+apiRoutes.use(cookieParser())
+var jwt = require('jsonwebtoken');
+var config = require('./config');
+
 
     // route middleware to verify a token
 apiRoutes.use(function(req, res, next) {
@@ -96,24 +36,7 @@ apiRoutes.use(function(req, res, next) {
 });
 
 
-app.route('/')
-    .get(function(req, res) {
-        var token = req.cookies.auth;
-        if (token) {
-            jwt.verify(token, app.get('superSecret'), function(err, token_data) {
-              if (err) {
-                res.redirect('/login');
-              } else {
-                // if everything is good, save to request for use in other routes
-                req.user_data = token_data;
-                redirect('/home');
-              }
-            });
-        }else{
-            res.redirect('/login');
-        }
 
-    });
 /*apiRoutes.get('/', function(req, res){
     res.json({message: 'Welcome to first version of wdp3chat-api'});
 });
@@ -124,7 +47,7 @@ apiRoutes.get('/users', function(req, res){
     });
 });
 
-apiRoutes.route('/home')
+apiRoutes.route('/chat')
     .get(function(req, res){
         res.send('Here is the main-page'+jwt.decode(req.cookies.auth));
         //console.log(jwt.decode(req.cookie));
--- server.js
+++ server.js
@@ -27,8 +27,91 @@ app.use(bodyParser.json());
 
 app.use(morgan('dev'));
 
+app.route('/')
+    .get(function(req, res) {
+        var token = req.cookies.auth;
+        if (token) {
+            jwt.verify(token, app.get('superSecret'), function(err, token_data) {
+              if (err) {
+                res.redirect('/login');
+              } else {
+                // if everything is good, save to request for use in other routes
+                req.user_data = token_data;
+                redirect('/chat');
+              }
+            });
+        }else{
+            res.redirect('/login');
+        }
+
+    });
+    app.route('/login')
+        .get(function(req, res){
+            res.sendFile(path.join(__dirname, 'views/login.html' ));
+        })
+        .put(function(req, res){
+            // find the user
+            console.log('PUT: ' + req.body.username);
+
+            User.findOne({name: req.body.username}, function(err, user) {
+
+              if (err) {
+                  //user not found
+                  return res.sent(401);
+              }
+
+              if (!user) {
+                //incorrect username
+                return res.sendStatus(401);
+                //res.json({ success: false, message: 'Authentication failed. User not found.' });
+              }
+              if (user.password != req.body.password) {
+                //incorrect password
+                return res.sendStatus(401);
+                // check if password matches
+                //  res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+              }
+              //res.sendStatus(200);
+                  // if user is found and password is right
+                  // create a token
+              var token = jwt.sign(user, app.get('superSecret'), {
+                expiresIn: '1440m' // expires in 24 hours
+              });
+              console.log(token);
+              //var decoded = jwt.decode(token);
+              //console.log(decoded);
+
+            res.cookie('auth',token);
+            res.send({message: 'ok'});
+            //res.send(req.body);
+          });
 
 
+        })
+        .post(function(req, res){
+            console.log('POST: ' + req.body.username);
+            User.findOne({'name': req.body.username}, function(err, user) {
+                if (err) throw err;
+
+                var newUser = new User({
+                  name: req.body.username,
+                  email: req.body.email,
+                  password: req.body.password,
+                  admin: false
+                });
+
+                if (user) {
+                    res.json({ success: false, message: 'Authentication failed. User already exists.' });
+                } else {
+                    newUser.save(function(err) {
+                      if (err) throw err;
+
+                      res.json({ success: true, message: 'User saved successfully' });
+                    });
+                }
+            });
+
+        });
 //start the server
 app.listen(port);
 console.log('Magic happens at http://127.0.0.1:' + port);
