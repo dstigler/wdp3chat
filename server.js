aa4a0f3a3405b69cec23da970290b90d465af9a5
dstigler <S1510307109@students.fh-hagenberg.at>
2017-01-02 23:49:00 +0100

    try

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.route('/login')
           //console.log(decoded);
 
         res.cookie('auth',token);
-        //res.send({message: 'ok'});
+        res.send({message: 'ok'});
         //res.redirect('/api/chat');
         //res.send(req.body);
       });
