0a62e33f917470ac00998ca670b54c3976eea1b5
dstigler <S1510307109@students.fh-hagenberg.at>
2017-01-02 13:34:18 +0100

    try

--- server.js
+++ server.js
@@ -162,6 +162,10 @@ app.use('/api', apiRoutes);
 
 
 apiRoutes.route('/logout')
+    .get(function(req, res){
+        res.clearCookie("auth");
+        res.redirect('/login');
+    })
     .delete(function(req, res){
         res.clearCookie("auth");
         res.redirect('/login');
