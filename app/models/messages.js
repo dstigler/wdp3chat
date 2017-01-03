66fdf397e51c0f6a4196bebc87fa150aed664d43
dstigler <S1510307109@students.fh-hagenberg.at>
2017-01-03 19:58:10 +0100

    try

--- app/models/messages.js
+++ app/models/messages.js
@@ -5,6 +5,10 @@ var Schema = mongoose.Schema;
 module.exports = mongoose.model('Message', new Schema({
     msg_datetime: Date,
     msg_text: String,
-    msg_chat_id: ObjectID,
-    msg_user_id: ObjectID
+    msg_chat_id:
+        {type: mongoose.Schema.ObjectID,
+         ref: 'Rooms'},
+    msg_user_id: {
+        type: mongoose.Schema.ObjectID,
+        ref: 'User'}
 }));
