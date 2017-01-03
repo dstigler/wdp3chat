128b8ea132f783f67f4ef5980c46249bb36619e8
dstigler <S1510307109@students.fh-hagenberg.at>
2017-01-03 18:12:13 +0100

    try

--- public/home.js
+++ public/home.js
@@ -10,7 +10,14 @@ $(function () {
         //getMessages();
         $.each(rooms, function (key, room) {
             console.log(rooms);
-            var a = '<li class="state-default"><a data-room-id="' + room._id + '" class="room list-group-item">' + room.chat_name + '</a></li>';
+            if(room.chat_name == "Mainchat"){
+                var a = '<li class="state-default ui-selected"><a data-room-id="' + room._id +
+                        '" class="room list-group-item">' + room.chat_name + '</a></li>';
+            }else{
+                var a = '<li class="state-default"><a data-room-id="' + room._id +
+                        '" class="room list-group-item">' + room.chat_name + '</a></li>';
+            }
+
             $("#rooms-sortable").append(a);
         });
 
