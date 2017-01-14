28d588c6a24a17ea4ca201d16a19cd8641577720
dstigler <S1510307109@students.fh-hagenberg.at>
2017-01-14 13:51:44 +0100

    try

--- public/home.js
+++ public/home.js
@@ -1,44 +1,8 @@
 var roomId;
 
 $(function () {
-    $.ajax({
-        type: "GET",
-        url: "/api/roomlist"
-    }).success(function (rooms) {
-        //roomId = rooms[0].chat_name;
-        //getMessages();
-        console.log(rooms);
-        var found = false;
-        var mainId;
-        $.each(rooms, function (key, room) {
-            if(room.chat_name == "Mainchat"){
-                mainId = room._id;
-            }
-            if(roomId == 'undefined' && room.chat_name == "Mainchat"){
-                var a = '<li class="state-default ui-selected" id="'+room._id+'"><a class="room list-group-item">' + room.chat_name + '</a></li>';
-                roomId = room._id;
-                //getMessages();
-                console.log(roomId);
-                found = true;
-            }else{
-                if(roomId == room._id){
-                    var a = '<li class="state-default ui-selected" id="'+room._id+'"><a class="room list-group-item">' + room.chat_name + '</a></li>';
-                    roomId = room._id;
-
-                    console.log(roomId);
-                    found = true;
-                }else{
-                    var a = '<li class="state-default" id="'+room._id+'"><a class="room list-group-item">' + room.chat_name + '</a></li>';
-                }
-            }
-            if(found == false){
-                roomId = mainId;
-                $("#"+roomId).addClass("ui-selected");
-            }
-            getMessages();
-            $("#rooms-sortable").append(a);
-        });
-    });
+    getRooms();
+    getMessages();
 });
 
 $("#rooms-sortable").click(function (){
@@ -101,10 +65,51 @@ function postMessage() {
                 messages += message.msg_user_name + ": " + message.msg_text + "\r";//message.text + "\r";
             });
             $("#chat-textarea").val(messages);
-            setTimeout(getMessages, 500);
+            setTimeout(getMessages, 1000);
         });
     };
 
+    function getRooms(){
+        $.ajax({
+            type: "GET",
+            url: "/api/roomlist"
+        }).success(function (rooms) {
+            //roomId = rooms[0].chat_name;
+            //getMessages();
+            console.log(rooms);
+            var found = false;
+            var mainId;
+            $("#rooms-sortable").empty();
+            $.each(rooms, function (key, room) {
+                if(room.chat_name == "Mainchat"){
+                    mainId = room._id;
+                }
+                if(roomId == 'undefined' && room.chat_name == "Mainchat"){
+                    var a = '<li class="state-default ui-selected" id="'+room._id+'"><a class="room list-group-item">' + room.chat_name + '</a></li>';
+                    roomId = room._id;
+                    //getMessages();
+                    console.log(roomId);
+                    found = true;
+                }else{
+                    if(roomId == room._id){
+                        var a = '<li class="state-default ui-selected" id="'+room._id+'"><a class="room list-group-item">' + room.chat_name + '</a></li>';
+                        roomId = room._id;
+
+                        console.log(roomId);
+                        found = true;
+                    }else{
+                        var a = '<li class="state-default" id="'+room._id+'"><a class="room list-group-item">' + room.chat_name + '</a></li>';
+                    }
+                }
+                if(found == false){
+                    roomId = mainId;
+                    $("#"+roomId).addClass("ui-selected");
+                }
+                $("#rooms-sortable").append(a);
+            });
+        });
+        setTimeout(getRooms, 10000);
+    };
 
 //setTimeout(getMessages, 750);
 document.getElementById("postMsgButton").onclick = postMessage;
