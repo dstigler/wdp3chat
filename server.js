59149084d844b45644cfcce8673f5de705a45eb4
dstigler <S1510307109@students.fh-hagenberg.at>
2017-01-25 17:21:40 +0100

    try

--- server.js
+++ server.js
@@ -80,9 +80,10 @@ app.route('/login')
     })
     .post(function(req, res){
         console.log('POST: ' + req.body.username);
-        User.findOne({'name': req.body.username}, function(err, user) {
+        var escName = escape(req.body.username);
+        User.findOne({'name':escName}, function(err, user) {
             if (err) throw err;
-            var escName = escape(req.body.username);
+
             var newUser = new User({
               name: escName,
               email: req.body.email,
